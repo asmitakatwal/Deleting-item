3fd107cfe4c907c2178655c0cf605042861d77f7
asmitakatwal <asmitakatwal246@gmail.com>
2023-09-11 08:28:57 +0545

    changes

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
-function App() {
+const App = () => {
+  const [arr, setArr] = useState(['ram', 'shyam', 'hari', 'geeta'])
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+
+
+      <h1>{arr.map((item, id) => {
+        return (
+          <li>{item}</li>
+        )
+      })}</h1>
+
+<button onClick={() =>{ 
+  const tempArr=[...arr]
+  tempArr.pop()
+setArr(tempArr)}}>Delete</button>
+
     </div>
   );
-}
 
+}
 export default App;
