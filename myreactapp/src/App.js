8a917a5fbcde4c2912191ccfea477903ad2e2ef8
asmitakatwal <asmitakatwal246@gmail.com>
2023-09-11 09:22:28 +0545

    only changes

--- myreactapp/src/App.js
+++ myreactapp/src/App.js
@@ -1,30 +1,59 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import { useState } from 'react';
+
+// const App = () => {
+//   const [arr, setArr] = useState(['ram', 'shyam', 'hari', 'geeta'])
+
+
+
+//   return (
+//     <div className="App">
+
+
+
+//       <h1>{arr.map((item, id) => {
+//         return (
+//           <li>{item}</li>
+//         )
+//       })}</h1>
+
+// <button onClick={() =>{ 
+//   const tempArr=[...arr]
+//   tempArr.pop()
+// setArr(tempArr)}}>Delete</button>
+
+//     </div>
+//   );
+
+// }
+// export default App;
+
+
+
+
+
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 
 const App = () => {
-  const [arr, setArr] = useState(['ram', 'shyam', 'hari', 'geeta'])
+  const [name,setName]=useState('kaylin')
+ useEffect(()=>{
+  setName('Asmita')
+ },[])
 
 
 
   return (
     <div className="App">
-
-
-
-      <h1>{arr.map((item, id) => {
-        return (
-          <li>{item}</li>
-        )
-      })}</h1>
-
-<button onClick={() =>{ 
-  const tempArr=[...arr]
-  tempArr.pop()
-setArr(tempArr)}}>Delete</button>
+<button onClick={()=>setName('ayush')}>Change</button>
+{/* //setName ma bracket lauda already first load mai render hunxa ani nalaida chai pachi hunxa */}
+<p>{name}</p>
 
     </div>
   );
 
 }
 export default App;
+
